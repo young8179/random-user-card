9489eac2c7ff0cb263722bec49f5cbfaa7830789
Heeyoung Song <hsong8179@gmail.com>
2020-10-26 14:40:34 -0400

    initial commit

---
+++ src/components/RandomUser.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class RandomUser extends Component {
+    render() {
+        return (
+            <div>
+                <p>hello</p>
+            </div>
+        )
+    }
+}
+
+
+export default RandomUser
